cc598f87e4d50edd0d7e44dd2a34dfdb3df84886
Samuel_910 <dasarupazfesandra@gmail.com>
2022-06-28 08:47:03 -0500

    Proyecto integrador

---
+++ ProyectoGUI1/src/main/java/pe/edu/upeu/modelo/Persona.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.modelo;
+
+public class Persona {
+    public String codigo;
+    public String nombre;
+    public boolean asistencia;
+
+    public Persona(String codigo, String nombre, boolean asistencia) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.asistencia = asistencia;
+    }
+}
